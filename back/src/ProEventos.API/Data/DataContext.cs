28b501b9b28aab37bdae8a8503ab57cdfeb142c4
NailsonFerreira <nailson_ferreira@hotmail.com>
2021-08-05 06:55:02 -0300

    Aula 185

---
+++ back/src/ProEventos.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base()
+        {
+            
+        }
+        public DbSet<Evento> Eventos { get; set; }
+    }
+}
