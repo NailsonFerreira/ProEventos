d5d44801ebf6a8e4783c65155903a92e50564242
NailsonFerreira <nailson_ferreira@hotmail.com>
2021-08-05 06:26:37 -0300

    Aula 181

---
+++ back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        public EventoController()
+        {
+            
+        }
+
+        [HttpGet]
+        public Evento Get()
+        {
+            return new Evento{
+                EventoId=0,
+                Tema= "Xuxa",
+                DataEvento=DateTime.Now.ToString(),
+                Local= "Piedade",
+                QuantidadePessoas=250,
+                Lote="36n",
+                ImagemUrl= "www.google.com"
+            };
+        }
+    }
+}
