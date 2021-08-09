f5e85fe171f622eb664bb68651e2a129bae9d47e
NailsonFerreira <nailson_ferreira@hotmail.com>
2021-08-09 00:02:20 -0300

    Seção 20: V2 - Angular - Interpolação, Diretivas e Binding

--- front/Proeventos-App/src/app/eventos/eventos.component.ts
+++ front/Proeventos-App/src/app/eventos/eventos.component.ts
@@ -10,17 +10,46 @@ export class EventosComponent implements OnInit {
 
   constructor(private http: HttpClient) { }
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFilter: any = [];
+  widthImg = 100;
+  marginImg = 2;
+  showImage = true;
+  private _search: string = '';
+
+  public get search(): string {
+    return this._search;
+  }
+
+  public set search(value: string) {
+    this._search = value;
+    this.eventosFilter = this._search ? this.filterEvents(value) : this.eventos;
+  }
 
   ngOnInit(): void {
     this.getEventos();
   }
 
-  public getEventos(): void{
+  public getEventos(): void {
     this.http.get("http://localhost:5000/evento").subscribe(
-      response => this.eventos = response,
+      (response) => {
+        this.eventos = response;
+        this.eventosFilter = response;
+        console.log(response);
+      },
       error => console.log(error),
     )
   }
 
+  public setShowImages(): void {
+    this.showImage = !this.showImage;
+  }
+
+  public filterEvents(value: string) {
+    value = value.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: { tema: string, local:string; }) => evento.tema.toLocaleLowerCase().indexOf(value) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(value) !== -1
+    );
+  }
 }
