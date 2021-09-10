9a6c79dc871555f13e8e7c490264b780bdb2ab0a
NailsonFerreira <nailson_ferreira@hotmail.com>
2021-09-09 23:28:23 -0300

    Aula 265

--- front/Proeventos-App/src/app/shared/titulo/titulo.component.ts
+++ front/Proeventos-App/src/app/shared/titulo/titulo.component.ts
@@ -8,6 +8,9 @@ import { Component, Input, OnInit } from '@angular/core';
 export class TituloComponent implements OnInit {
 
   @Input() titulo!: string;
+  @Input() subTitulo = "Desde 2021";
+  @Input() iconClass = "fa fa-user";
+  @Input() botaoListar = false;
   constructor() { }
 
   ngOnInit() {
